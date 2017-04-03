d0033a744f08791b237e7bc0860ab90c152d7cab
Simon Tong <espwnwn@gmail.com>
2017-04-03 22:04:33 +0300

    Update core.py

--- core.py
+++ core.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd.read_csv('.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
